2e99afe2dfc99287f1dd2e15e3f71a7040dc5e9f
unknown <salerysofficial@gmail.com>
2024-06-05 18:11:44 +0200

    added part to get Username

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_username():
+    #   Ask and validate the user's name
+    username = input("Welcome to the number guessing game! How can I call you?\n")
+
+    while not username.isalpha():
+        username = input(
+            "Your name can only contain letters. Please tell me your name correctly!\n"
+        )
+
+    if username.isalpha():
+        print(username)
+
+get_username()
+    
